7b6640a1106a7a0bc31e9ca8b4e0b5eac4231d92
Kumar Mayank <kumarmayank@Kumars-MacBook-Pro.local>
2022-05-03 19:40:55 +0530

    name mapping code

---
+++ src/main/java/TimsheetTasks/FetchingDataFromExcel.java
@@ -0,0 +1,63 @@
+package TimsheetTasks;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class FetchingDataFromExcel {
+
+    public static void main(String[] args) throws IOException {
+        createHashMap(findColumnValue(getSheet()),getSheet());
+    }
+
+    public static void createHashMap(int[] arr, XSSFSheet sheet) {
+        HashMap<String, String> hashMap1 = new HashMap<String, String>();
+        HashMap<String, String> hashMap2 = new HashMap<String, String>();
+
+
+        for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+            XSSFRow row = sheet.getRow(i);
+            String key = row.getCell(arr[0]).getStringCellValue();
+            String Value = row.getCell(arr[1]).getStringCellValue();
+            hashMap1.put(key, Value);
+            hashMap2.put(Value, key);
+        }
+        System.out.println(hashMap1.toString());
+        System.out.println(hashMap2.toString());
+    }
+
+
+    public static int[] findColumnValue(XSSFSheet sheet) throws IOException {
+        int[] arr = new int[2];
+        XSSFRow row;
+
+        row = sheet.getRow(0);
+        for (int i = 0; i < row.getLastCellNum(); i++) {
+            if (row.getCell(i).getStringCellValue().equalsIgnoreCase("EMP_NAME")) {
+                arr[0] = i;
+                break;
+            }
+        }
+        for (int j = 0; j < row.getLastCellNum(); j++) {
+            if (row.getCell(j).getStringCellValue().equalsIgnoreCase("FG_NAME")) {
+                arr[1] = j;
+                break;
+            }
+        }
+        return arr;
+    }
+
+
+    public static XSSFSheet getSheet() throws IOException {
+        FileInputStream fis = new FileInputStream("/Users/kumarmayank/Documents/DemoData.xlsx");
+        XSSFWorkbook workbook = new XSSFWorkbook(fis);
+        XSSFSheet sheet = workbook.getSheet("Sheet1");
+        return sheet;
+    }
+}
